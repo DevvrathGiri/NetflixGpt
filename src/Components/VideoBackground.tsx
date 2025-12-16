e2c02820b9122fa01f191d7f9240a950d8a4084d
DevvrathGiri <giri64563@gmail.com>
2025-12-16 16:22:56 +0530

    created different hook of useMovieTrailer to keep my video background code clean

--- src/Components/VideoBackground.tsx
+++ src/Components/VideoBackground.tsx
@@ -1,27 +1,11 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../Utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../Utils/movieSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../Hooks/useMovieTrailer";
+
 
 const VideoBackground = ({ movieId }) => {
     const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/976573/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter((video) => video.type == "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+    useMovieTrailer();
+ 
   return (
     <div>
       <iframe
