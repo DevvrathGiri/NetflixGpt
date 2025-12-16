d1f9f4f8d893a1bd7abb9a91feaa0ed510ee0670
DevvrathGiri <giri64563@gmail.com>
2025-12-16 15:27:21 +0530

    read movie data with useSelector

--- src/Components/MainContainer.tsx
+++ src/Components/MainContainer.tsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import VideoBackground from "./VideoBackground";
 import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
+    const movies = useSelector((store)=>store.movies?.nowPlayingMovies);
+    if(!movies) return;
+
+    const mainMovie = movies[0];
+    console.log(mainMovie);
   return (
     <div>
    <VideoTitle/>
