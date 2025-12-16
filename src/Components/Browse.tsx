a1f5318bb2a1eafc00dd04a9b27fd696de100302
DevvrathGiri <giri64563@gmail.com>
2025-12-16 15:17:32 +0530

    cleaned browse page by creating hook for now playingmovies

--- src/Components/Browse.tsx
+++ src/Components/Browse.tsx
@@ -1,16 +1,6 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../Utils/constants"
 import Header from "./Header"
-const Browse = () => {
-  const getNowPlayingMovies = async () =>{
-    const data  = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json);
-  }
 
-  useEffect(()=>{
-    getNowPlayingMovies();
-  }, [])
+const Browse = () => {
   return (
     <div>
       <Header/>
---
+++ src/Hooks/useNowPlayingMovies.tsx
@@ -0,0 +1,19 @@
+import { addNowPlayingMovies } from "../Utils/movieSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../Utils/constants"
+const useNowPlayingMovies = () =>{
+      const Dispatch = useDispatch();
+  const getNowPlayingMovies = async () =>{
+    const data  = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+    const json = await data.json();
+    console.log(json.results);
+    Dispatch(addNowPlayingMovies(json.results));
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  }, [])
+}
+
+export default useNowPlayingMovies;
