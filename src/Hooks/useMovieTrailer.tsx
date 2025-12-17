ac518b4da84c7e5c82a1b81ac8820e503f962754
DevvrathGiri <giri64563@gmail.com>
2025-12-17 15:09:03 +0530

    removed ts errors

--- src/Hooks/useMovieTrailer.tsx
+++ src/Hooks/useMovieTrailer.tsx
@@ -3,24 +3,40 @@ import { API_OPTIONS } from "../Utils/constants";
 import { useDispatch } from "react-redux";
 import { addTrailerVideo } from "../Utils/movieSlice";
 
-const useMovieTrailer = (movieId) => {
- const dispatch = useDispatch();
+/* 🔥 ADD TYPE HERE */
+interface Video {
+  id: string;
+  key: string;
+  name: string;
+  type: string;
+}
+
+const useMovieTrailer = (movieId: number) => {
+  const dispatch = useDispatch();
+
   const getMovieVideos = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US",
+      "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US",
       API_OPTIONS
     );
+
     const json = await data.json();
     console.log(json);
-    const filterData = json.results.filter((video) => video.type == "Trailer");
+
+    /* 🔥 ADD TYPE HERE */
+    const filterData = json.results.filter(
+      (video: Video) => video.type === "Trailer"
+    );
+
     const trailer = filterData.length ? filterData[0] : json.results[0];
     console.log(trailer);
+
     dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
     getMovieVideos();
   }, []);
-}
+};
 
-export default useMovieTrailer
+export default useMovieTrailer;
